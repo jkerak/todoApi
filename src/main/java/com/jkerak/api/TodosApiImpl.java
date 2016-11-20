b38fad2f41b2979412500b247310b2a2347d7a04
James Kerak <jkerak@001830-mac.local>
2016-11-20 14:57:08 -0500

    TodosApiImpl skeleton

---
+++ src/main/java/com/jkerak/api/TodosApiImpl.java
@@ -0,0 +1,10 @@
+package com.jkerak.api;
+import com.jkerak.Todo;
+import com.jkerak.TodosApi;
+
+public class TodosApiImpl implements TodosApi {
+
+    public Todo todosIdGet(Long id) {
+        return null;
+    }
+}
